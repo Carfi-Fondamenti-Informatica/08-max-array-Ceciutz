120ee3a4468996baca58c2cee848c6fcc425846d
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-12-13 11:00:59 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Cecilia Utzeri on 13/12/2022.
 //
 
-#ifndef ESERCITAZIONE_8_LIB_H
-#define ESERCITAZIONE_8_LIB_H
+#ifndef ESERCITAZIONE_8
+#define ESERCITAZIONE_8
 float array(float a[], int d);
 #endif //ESERCITAZIONE_8_LIB_H
