41532dfc40cce2bd00743c98a45d628b27bf6266
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-12-13 09:57:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by Cecilia Utzeri on 13/12/2022.
+//
 
+#include "lib.h"
+float array(float a[], int d){
+    for(int i=0;i<d;i++){
+        if(a[0]<=a[i]){
+            a[0]=a[i];
+        }else{
+            a[i]=a[0];
+        }
+    }
+    return a[0];
+}
