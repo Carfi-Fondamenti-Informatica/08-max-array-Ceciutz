641b5846cfa0c7361548ec3128f29f47872fb80f
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-12-13 09:58:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int N;
+    cin>>N;
+    float numeri[N];
+    for(int i=0;i<N;i++){
+        cin>>numeri[i];
+    }
+    cout<<array(numeri,N)<<endl;
   return 0;
 }
